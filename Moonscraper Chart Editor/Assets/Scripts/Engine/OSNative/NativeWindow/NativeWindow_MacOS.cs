4919c95abfa2ecde8c7fad7f1ae0262f8f1689a6
Thiago Kuhn <thiago.kuhn@supercompany.io>
2023-11-22 13:33:25 -0300

    Fix errors on Mac build because of lack of native window support

---
+++ Moonscraper Chart Editor/Assets/Scripts/Engine/OSNative/NativeWindow/NativeWindow_MacOS.cs
@@ -0,0 +1,21 @@
+﻿#if UNITY_STANDALONE_OSX
+
+public class NativeWindow_MacOS : INativeWindow
+{
+    public bool IsConnectedToWindow()
+    {
+        return true;
+    }
+
+    public bool SetApplicationWindowPointerByName(string desiredWindowName)
+    {
+        return true;
+    }
+
+    public void SetWindowTitle(string title)
+    {
+        // Not supported
+    }
+}
+
+#endif
